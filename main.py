c819321cb9875caefc6a709450e9d36146c8df83
tommyrharper <thomasrobertharper@gmail.com>
2025-02-24 15:50:28 +0000

    feat: tidy according to ruff formatting rules

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+"""Speech recognition using OpenAI's Whisper model."""
+
+import torch
 import whisper
 
-def main():
-    print("Hello from mlxweek5!")
 
-    model = whisper.load_model("tiny.en")
+def main(device: str = "cpu") -> None:
+    """Initialize and load Whisper model on specified device."""
+
+    model = whisper.load_model("tiny.en", device=device)
 
 
 if __name__ == "__main__":
-    main()
+    device = (
+        "mps"
+        if torch.backends.mps.is_available()
+        else "cuda"
+        if torch.cuda.is_available()
+        else "cpu"
+    )
+    main(device)
