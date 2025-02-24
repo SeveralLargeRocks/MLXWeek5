3baf3730c9aa3524ed050fe2a20a7ffefd644913
tommyrharper <thomasrobertharper@gmail.com>
2025-02-24 15:59:03 +0000

    feat: transcribe hello.wav

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main(device: str = "cpu") -> None:
     """Initialize and load Whisper model on specified device."""
 
     model = whisper.load_model("tiny.en", device=device)
+    model.eval()
+
+    audio_path = "hello.wav"
+    result = model.transcribe(audio_path)
+    print("Transcription: ",result["text"])
 
 
 if __name__ == "__main__":
