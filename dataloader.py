581f4c60fb55a7cdfb463757e919ab699e9c8f5e
tommyrharper <thomasrobertharper@gmail.com>
2025-02-25 14:10:36 +0000

    feat: return file and text from dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,4 +1,4 @@
-import torch
+import aiohttp
 from datasets import load_dataset
 from torch.utils.data import Dataset, DataLoader
 
@@ -11,7 +11,10 @@ class LibriSpeechDataset(Dataset):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        return self.dataset[idx]
+        item = self.dataset[idx]
+        file = item["file"]
+        text = item["text"]
+        return file, text
 
 
 def load_librispeech():
@@ -20,14 +23,20 @@ def load_librispeech():
         name="clean",
         split="train.100",
         cache_dir="./data",
-        streaming=True,
+        trust_remote_code=True,
+        storage_options={
+            "client_kwargs": {"timeout": aiohttp.ClientTimeout(total=3600)}
+        },
     )
     valid_dataset = load_dataset(
         path="librispeech_asr",
         name="clean",
         split="validation",
         cache_dir="./data",
-        streaming=True,
+        trust_remote_code=True,
+        storage_options={
+            "client_kwargs": {"timeout": aiohttp.ClientTimeout(total=3600)}
+        },
     )
 
     return LibriSpeechDataset(train_dataset), LibriSpeechDataset(valid_dataset)
@@ -37,9 +46,10 @@ if __name__ == "__main__":
     batch_size = 2
     train_dataset, valid_dataset = load_librispeech()
 
-    # train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
-    # valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size)
 
     print("im done bro")
-    # batch = next(iter(train_dataloader))
-    # print(batch)
+    files, items = next(iter(train_dataloader))
+    print(files)
+    print(items)
